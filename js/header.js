1c2ec950de998028ed89b087041d521e9083a08f
jstutul <jaforsadiktutul9228@gmail.com>
2023-10-11 01:29:26 +0600

    nav

--- js/header.js
+++ js/header.js
@@ -1,3 +1,8 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
 // sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
@@ -18,7 +23,7 @@ htmlcssArrow.onclick = function () {
 // moreArrow.onclick = function () {
 //     navLinks.classList.toggle("show2");
 // };
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function () {
-    navLinks.classList.toggle("show3");
-};
+// let jsArrow = document.querySelector(".js-arrow");
+// jsArrow.onclick = function () {
+//     navLinks.classList.toggle("show3");
+// };
--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
-  var includes = $("[data-include]");
-  $.each(includes, function () {
-    var file = "includes/" + $(this).data("include") + ".html";
-    $(this).load(file);
-  });
+    var includes = $("[data-include]");
+    $.each(includes, function () {
+        var file = "includes/" + $(this).data("include") + ".html";
+        $(this).load(file);
+    });
+    $(document).on("change", "#txtResultType", function () {
+        var resultType = $(this).find("option:selected").val();
+
+        if (resultType != 0) {
+            $("#txtRollNumber").removeAttr("disabled").removeAttr("readonly");
+        } else {
+            $("#txtRollNumber")
+                .val("")
+                .attr("disabled", true)
+                .attr("readonly", true);
+        }
+    });
 });
