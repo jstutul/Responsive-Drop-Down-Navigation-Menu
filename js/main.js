05ce36bd66fd4b3cd102422fb5e366d886874de8
jstutul <jaforsadiktutul9228@gmail.com>
2023-10-12 00:54:39 +0600

    navbar done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ $(document).ready(function () {
         var file = "includes/" + $(this).data("include") + ".html";
         $(this).load(file);
     });
+    $(document).on("click", "#menuBtn", function () {
+        $(".nav-links").css("left", "0");
+    });
+
+    $(document).on("click", "#menuClose", function () {
+        $(".nav-links").css("left", "-100%");
+    });
+
+    $(document).on("click", ".links li", function () {
+        $(this).find("ul.sub-menu").toggleClass("d-block");
+        $(this).find(".bx").toggleClass("rotate-180");
+    });
+
     $(document).on("change", "#txtResultType", function () {
         var resultType = $(this).find("option:selected").val();
 
